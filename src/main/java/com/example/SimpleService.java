e29184a603f0c3e8867d465f13060243cbeb46e2
Viktor <sfaxi19@localhost.localdomain>
2016-02-09 19:51:34 +0300

    merge

--- src/main/java/com/example/SimpleService.java
+++ src/main/java/com/example/SimpleService.java
@@ -3,14 +3,22 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class SimpleService {
+public class SimpleService extends SpringBootServletInitializer {
     
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SimpleService.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SimpleService.class);
+    }
 }
